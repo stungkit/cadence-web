6e3f65f1dd5c9e330ac45baf3a9d38f802a31fab
Adhitya Mamallan <adhitya.mamallan@uber.com>
2025-05-21 16:45:48 +0200

    Add word-break for table columns (#915)
    Add word-break for table cells to fix styling for abnormally long cell values

--- src/components/table/table-body-cell/table-body-cell.styles.ts
+++ src/components/table/table-body-cell/table-body-cell.styles.ts
@@ -4,5 +4,6 @@ import { StyledTableBodyCell } from 'baseui/table-semantic';
 export const styled = {
   TableBodyCell: withStyle<typeof StyledTableBodyCell>(StyledTableBodyCell, {
     verticalAlign: 'middle',
+    wordBreak: 'break-word',
   }),
 };
