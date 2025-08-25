3df92872b78de51258c5daf4fa167b90e6bfd7a0
Assem Hafez <137278762+Assem-Uber@users.noreply.github.com>
2025-08-25 15:40:43 +0200

    add node runtime instrumentation (#997)
    Co-authored-by: Adhitya Mamallan <adhitya.mamallan@uber.com>

--- src/utils/otel/otel-register-instrumentations.ts
+++ src/utils/otel/otel-register-instrumentations.ts
@@ -11,6 +11,10 @@ import {
   type PinoInstrumentationConfig,
 } from '@opentelemetry/instrumentation-pino';
 import {
+  RuntimeNodeInstrumentation,
+  type RuntimeNodeInstrumentationConfig,
+} from '@opentelemetry/instrumentation-runtime-node';
+import {
   UndiciInstrumentation,
   type UndiciInstrumentationConfig,
 } from '@opentelemetry/instrumentation-undici';
@@ -20,16 +24,19 @@ export function otelRegisterInstrumentations({
   httpInstrumentationConfig,
   undiciInstrumentationConfig,
   pinoInstrumentationConfig,
+  runtimeNodeInstrumentationConfig,
 }: {
   grpcInstrumentationConfig?: GrpcInstrumentationConfig;
   httpInstrumentationConfig?: HttpInstrumentationConfig;
   undiciInstrumentationConfig?: UndiciInstrumentationConfig;
   pinoInstrumentationConfig?: PinoInstrumentationConfig;
+  runtimeNodeInstrumentationConfig?: RuntimeNodeInstrumentationConfig;
 } = {}) {
   return [
     new GrpcInstrumentation(grpcInstrumentationConfig),
     new HttpInstrumentation(httpInstrumentationConfig),
     new UndiciInstrumentation(undiciInstrumentationConfig),
     new PinoInstrumentation(pinoInstrumentationConfig),
+    new RuntimeNodeInstrumentation(runtimeNodeInstrumentationConfig),
   ];
 }
