cf827a51934c45a60fe1829126f9b34c363c53e6
Chris Warren <16132615+c-warren@users.noreply.github.com>
2025-12-11 08:13:13 -0800

    fix: Stop failover history from looping when reaching the end of results (#1113)
    When fetching failover history and reaching the end of results (a response of nextPageToken: "") the page would attempt to load the next page of results. An empty token/undefined token is assumed to be the first page by the server, which resulted in an infinite loop of results.
    
    This update ensures that the nextPageToken is only populated if it is a non-empty string.

--- src/views/domain-page/hooks/use-domain-failover-history/get-domain-failover-history-query-options.ts
+++ src/views/domain-page/hooks/use-domain-failover-history/get-domain-failover-history-query-options.ts
@@ -27,7 +27,7 @@ export default function getDomainFailoverHistoryQueryOptions(
         })
       ).then((res) => res.json()),
     initialPageParam: undefined,
-    getNextPageParam: (res) => res.nextPageToken,
+    getNextPageParam: (res) => res.nextPageToken || undefined,
     retry: false,
   };
 }
