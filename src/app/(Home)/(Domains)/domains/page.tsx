0f8b33d4925152395c4beaaea77541727fce3f01
Adhitya Mamallan <adhitya.mamallan@uber.com>
2026-09-17 13:24:29 +0200

    fix: Opt back in to dynamic rendering on domains page to fix build issues (#1538)
    ## Summary
    ### Problem 
    #1524 removed the `force-dynamic` flag from the Domains page after
    opting it in to client rendering. However, this breaks production builds
    which attempt to pre-render the Domains page even though it has dynamic
    content.
    
    ### Solution
    Add `force-dynamic` back to the Domains page.
    
    ## Test plan
    `npm run build` passes.
    
    Signed-off-by: Adhitya Mamallan <adhitya.mamallan@uber.com>

--- src/app/(Home)/(Domains)/domains/page.tsx
+++ src/app/(Home)/(Domains)/domains/page.tsx
@@ -3,6 +3,8 @@ import { type Metadata } from 'next';
 import DomainsPage from '@/views/domains-page/domains-page';
 import { domainsPageMetadata } from '@/views/domains-page/domains-page.metadata';
 
+export const dynamic = 'force-dynamic';
+
 export const metadata: Metadata = domainsPageMetadata;
 
 export default DomainsPage;
